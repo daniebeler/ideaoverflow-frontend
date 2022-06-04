aa72126624c18c2b58fd72e76cd3991f7562ed43
daniebeler <hiebeler.daniel@gmail.com>
2022-06-04 23:23:12 +0200

    upgraded to Angular 14

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,30 +16,29 @@ import { ComponentsModule } from './components/components.module';
 import { ErrorInterceptor } from './interceptors/error.interceptor';
 
 @NgModule({
-  declarations: [AppComponent, ExternalHrefPipe],
-  entryComponents: [],
-  imports: [
-    BrowserModule.withServerTransition({ appId: 'serverApp' }),
-    ComponentsModule,
-    IonicModule.forRoot({ mode: 'md' }),
-    AppRoutingModule,
-    HttpClientModule,
-    QuillModule.forRoot(),
-    ServiceWorkerModule.register('ngsw-worker.js', {
-      enabled: environment.production,
-      registrationStrategy: 'registerImmediately'
-    }),
-    ServiceWorkerModule.register('ngsw-worker.js', {
-      enabled: environment.production,
-      // Register the ServiceWorker as soon as the app is stable
-      // or after 30 seconds (whichever comes first).
-      registrationStrategy: 'registerWhenStable:30000'
-    })
-  ],
-  providers: [
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true }
-  ],
-  bootstrap: [AppComponent],
+    declarations: [AppComponent, ExternalHrefPipe],
+    imports: [
+        BrowserModule.withServerTransition({ appId: 'serverApp' }),
+        ComponentsModule,
+        IonicModule.forRoot({ mode: 'md' }),
+        AppRoutingModule,
+        HttpClientModule,
+        QuillModule.forRoot(),
+        ServiceWorkerModule.register('ngsw-worker.js', {
+            enabled: environment.production,
+            registrationStrategy: 'registerImmediately'
+        }),
+        ServiceWorkerModule.register('ngsw-worker.js', {
+            enabled: environment.production,
+            // Register the ServiceWorker as soon as the app is stable
+            // or after 30 seconds (whichever comes first).
+            registrationStrategy: 'registerWhenStable:30000'
+        })
+    ],
+    providers: [
+        { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+        { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true }
+    ],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
