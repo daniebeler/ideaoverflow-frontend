c67ef54741430fd89c22200de4cfd53bf5578f4f
daniebeler <hiebeler.daniel@gmail.com>
2022-06-15 13:48:33 +0200

    fixed ideaPage

--- src/app/pages/idea/idea.page.ts
+++ src/app/pages/idea/idea.page.ts
@@ -1,9 +1,11 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ToastController } from '@ionic/angular';
 import { Subscription } from 'rxjs';
 import { Idea } from 'src/app/models/idea';
 import { User } from 'src/app/models/user';
 import { ApiService } from 'src/app/services/api.service';
+import { IdeaService } from 'src/app/services/idea.service';
 import { UserService } from 'src/app/services/user.service';
 
 @Component({
@@ -23,7 +25,9 @@ export class IdeaPage implements OnInit, OnDestroy {
   constructor(
     private activatedRoute: ActivatedRoute,
     private apiService: ApiService,
-    private userService: UserService
+    private userService: UserService,
+    private ideaService: IdeaService,
+    private toastController: ToastController
   ) { }
 
   ngOnInit() {
@@ -41,6 +45,64 @@ export class IdeaPage implements OnInit, OnDestroy {
       this.currentUser = latestUser;
     });
     this.subscriptions.push(subscription2);
+  }
+
+  votePost(voteValue: number, idea: Idea) {
+    if (this.currentUser) {
+      this.ideaService.voteIdea(voteValue, idea.id, this.currentUser.id);
+      if (idea.currentUserVoteValue === -1) {
+        if (voteValue === 0) {
+          idea.numberOfDownvotes--;
+        } else if (voteValue === 1) {
+          idea.numberOfDownvotes--;
+          idea.numberOfUpvotes++;
+        }
+      } else if (idea.currentUserVoteValue === 0) {
+        if (voteValue === -1) {
+          idea.numberOfDownvotes++;
+        } else if (voteValue === 1) {
+          idea.numberOfUpvotes++;
+        }
+      } else if (idea.currentUserVoteValue === 1) {
+        if (voteValue === -1) {
+          idea.numberOfDownvotes++;
+          idea.numberOfUpvotes--;
+        } else if (voteValue === 0) {
+          idea.numberOfUpvotes--;
+        }
+      }
+      idea.currentUserVoteValue = voteValue;
+    } else {
+      this.presentToast('You have to be logged in to vote ideas');
+    }
+  }
+
+  savePost(postId: number) {
+    if (this.currentUser) {
+      this.ideaService.saveIdea(postId, this.currentUser.id);
+      this.idea.saved = true;
+    } else {
+      this.presentToast('You have to be logged in to save Ideas');
+    }
+  }
+
+  unsavePost(postId: number) {
+    if (this.currentUser) {
+      this.ideaService.unsaveIdea(postId, this.currentUser.id);
+      this.idea.saved = false;
+    } else {
+      this.presentToast('You have to be logged in to save Ideas');
+    }
+  }
+
+  async presentToast(message: string) {
+    const toast = await this.toastController.create({
+      message,
+      icon: 'information-circle',
+      color: 'primary',
+      duration: 2500
+    });
+    toast.present();
   }
 
   ngOnDestroy(): void {
